994e42bf493718a232252eb1e6f9c01bcea0085e
平凡秀 <3549248116@qq.com>
2024-11-29 07:35:11 +0800

    Add files via upload

--- js/desktop.js
+++ js/desktop.js
@@ -2,7 +2,7 @@ $(function(){
     var tools=[
         {name:'此电脑',url:'http://sj.ysupan.com/8885155#/',icon:'./img/imageres_2.ico'},
         {name:'浏览器',url:'./bing.html',icon:'./img/k.webp'},
-        {name:'早报',url:'https://www.woshipm.com/news',icon:'https://picabstract-preview-ftn.weiyun.com/ftn_pic_abs_v3/556197ac6b5b89689ce060a10ca77dde6727e49c17ffe14dba03cea9ea60a53033aa9f0c234362b44d8bd4c9491ad7de?pictype=scale&from=30111&version=3.3.3.3&fname=u%3D2015335363%2C3143986395%26fm%3D253%26fmt%3Dauto%26app%3D138%26f%3DJPEG.webp&size=1024'},
+        {name:'早报',url:'http://hzyo.cn',icon:'https://picabstract-preview-ftn.weiyun.com/ftn_pic_abs_v3/556197ac6b5b89689ce060a10ca77dde6727e49c17ffe14dba03cea9ea60a53033aa9f0c234362b44d8bd4c9491ad7de?pictype=scale&from=30111&version=3.3.3.3&fname=u%3D2015335363%2C3143986395%26fm%3D253%26fmt%3Dauto%26app%3D138%26f%3DJPEG.webp&size=1024'},
         {name:'生成海报',url:'http://lm.05321888.com:88/hkzs/page/hb/index.html',icon:'https://picabstract-preview-ftn.weiyun.com/ftn_pic_abs_v3/127f2675dd321c9f7f9ec6bcf5c9df245d1531d7390cac59a56a354f96b2d5558d4784995acd4cbdbd9c12f29dcd63bc?pictype=scale&from=30111&version=3.3.3.3&fname=PicsArt_05-28-07.05.21.jpg&size=1024'},
         {name:'流量卡网厅',url:'/llk',icon:'./img/llkk.png'},
         {name:'代理注册',url:'./llk/dl.html',icon:'https://picabstract-preview-ftn.weiyun.com/ftn_pic_abs_v3/43c244d52c9d2eb76b5163287bde0c3acab9474b09d25c36c67a837fa60a0b9e2d418ab8392fa120f1b5463dc64e5dce?pictype=scale&from=30111&version=3.3.3.3&fname=PicsArt_05-28-09.01.21.jpg&size=1024'},
@@ -15,8 +15,8 @@ $(function(){
         {name:'游戏',url:'https://xingye.me/game/index.php',icon:'./img/g.webp'},
         {name:'影视',url:'https://youku.xm08.cn/',icon:'./img/h.webp'},
         {name:'站长工具箱',url:'//tool.hzyo.cn',icon:'./img/tool.png'},
-        {name:'音乐',url:'https://tools.liumingye.cn/music/?page=searchPage#/recent',icon:'https://picabstract-preview-ftn.weiyun.com/ftn_pic_abs_v3/a89916f469fd6d47b88662c73f94fec26a624d62ef514c8790a392906b2b3875f78629d6a6a6e50bff239b64ac8dbd14?pictype=scale&from=30111&version=3.3.3.3&fname=u%3D3242640730%2C2816196037%26fm%3D253%26fmt%3Dauto%26app%3D138%26f%3DJPEG.webp&size=1024'},
-        {name:'毒日记',url:'http://20.re/h5.html#/',icon:'http://20.re/favicon.ico'},
+        {name:'音乐',url:'http://mp3.hzyo.cn/',icon:'https://picabstract-preview-ftn.weiyun.com/ftn_pic_abs_v3/a89916f469fd6d47b88662c73f94fec26a624d62ef514c8790a392906b2b3875f78629d6a6a6e50bff239b64ac8dbd14?pictype=scale&from=30111&version=3.3.3.3&fname=u%3D3242640730%2C2816196037%26fm%3D253%26fmt%3Dauto%26app%3D138%26f%3DJPEG.webp&size=1024'},
+        {name:'赞助支持',url:'https://hzyo.cn/project/zz/',icon:'./img/6.png'},
 
     ]
     for(var i in tools){
